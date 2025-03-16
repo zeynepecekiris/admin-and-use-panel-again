3d0a92084b943959b46139116af4eb6aa17d4a06
zeynepecekiris <zeynep.ece933@gmail.com>
2025-03-16 22:29:06 +0300

    created tables for db

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import sqlite3
 import bcrypt
 import os
+import datetime import datetime
 
 
 app = Flask(__name__)
@@ -10,6 +11,35 @@ CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
 DB_PATH = os.path.join(BASE_DIR, "users.db") 
+
+def create_tables():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+
+    cursor.execute("""  
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    role TEXT NOT NULL
+    )""")
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    role TEXT NOT NULL
+     login_time TEXT NOT NULL
+    )""")
+
+
+    conn.commit()
+    conn.close()
+
+create_tables()
 
 def create_users_table():
     conn = sqlite3.connect(DB_PATH)
